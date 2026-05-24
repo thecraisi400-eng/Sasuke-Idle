acda55cb2a069ec84ad5628de9fde30213128748
thecraisi400-eng <thecraisi400@gmail.com>
2026-05-23 20:19:44 -0400

    Add files via upload

---
+++ js-modular/ui/toasts.js
@@ -0,0 +1,12 @@
+  nextLevelBannerTimeout = setTimeout(() => banner.classList.remove('show'), 1100);
+}
+
+function showToast(msg) {
+  const t = document.getElementById('toast');
+  t.textContent = msg;
+  t.classList.add('show');
+  clearTimeout(toastTimeout);
+  toastTimeout = setTimeout(() => t.classList.remove('show'), 2500);
+}
+
+
