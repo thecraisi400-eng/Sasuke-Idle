f5d401804748d8bdc8f16a4e685a3db2635839fe
thecraisi400-eng <thecraisi400@gmail.com>
2026-05-24 10:39:01 -0400

    99

--- js/app.js
+++
@@ -1,2 +0,0 @@
-// Punto de entrada para mantener el orden de carga de los módulos actuales.
-// No altera la lógica existente del juego.
--- js/axe/axe-upgrades.entry.js
+++
@@ -1,6 +0,0 @@
-// Punto de entrada del módulo "MEJORAR HACHA".
-// Este archivo existe para documentar/centralizar la carga modular en este orden:
-// 1) axe-upgrades.data.js
-// 2) axe-upgrades.calc.js
-// 3) axe-upgrades.actions.js
-// 4) axe-upgrades.render.js
